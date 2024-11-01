329508f46db9b9717b7b0dc3fd30bb88db4346a2
MsKing <dasung56@gmail.com>
2024-11-02 01:26:54 +0900

    몬스터 -> 오브 이동 구현

---
+++ Assets/Scripts/GamePlay/Enemy.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody2D target;
+
+    bool isLive;
+
+    Rigidbody2D EnemyRigid;
+    SpriteRenderer spriteRenderer;
+
+    void Awake()
+    {
+        EnemyRigid = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+
+    void FixedUpdate()
+    {
+        Vector2 dirVec = target.position - EnemyRigid.position; //방향
+        Vector2 nextVec = dirVec.normalized * speed * Time.fixedDeltaTime;
+        EnemyRigid.MovePosition(EnemyRigid.position + nextVec);
+        EnemyRigid.velocity = Vector2.zero;
+    }
+}
